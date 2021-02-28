4c7f54e649776f541cd66f56da54b281a675874d
Jens Ihrig <jens.ihrig@mail.de>
2021-02-28 17:41:05 +0100

    renamed ____ to __l__

--- __c__/index.php
+++ __c__/index.php
@@ -9,28 +9,27 @@ $dbchar = 'utf8';
 $dbuser = 'web247';
 $dbpass = 'Anzbe64G';
 try {
-  $pdo = new PDO(
-    "mysql:host=$dbhost;dbname=$dbname;charset=$dbchar",
-    $dbuser, $dbpass, 
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-  );
+    $pdo = new PDO(
+        "mysql:host=$dbhost;dbname=$dbname;charset=$dbchar",
+        $dbuser, $dbpass,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
 } catch (Exception $ex) {
-  die($ex->getMessage());
+    die($ex->getMessage());
 }
-
 
 // (B) SHOW COMMENTS
 if ($_GET['sid'] && ($_GET['pid'])) {
     // (B1) GET ALL COMMENTS
     try {
-      $stmt = $pdo->prepare("SELECT `name`, `timestamp`, `message` FROM `comments` WHERE `section_id`=? AND `post_id`=? ORDER BY `timestamp` ASC");
-     $stmt->execute([$_GET['sid'], $_GET['pid']]);
+        $stmt = $pdo->prepare("SELECT `name`, `timestamp`, `message` FROM `comments` WHERE `section_id`=? AND `post_id`=? ORDER BY `timestamp` ASC");
+        $stmt->execute([$_GET['sid'], $_GET['pid']]);
     } catch (Exception $ex) {
-      die($ex->getMessage());
+        die($ex->getMessage());
     }
 
     // (B2) LOOP & GENERATE HTML
-    while ($r = $stmt->fetch(PDO::FETCH_NAMED)) { ?>
+    while ($r = $stmt->fetch(PDO::FETCH_NAMED)) {?>
     <div class="crow">
       <div class="chead">
         <div class="cname"><?=$r['name']?></div>
@@ -41,23 +40,21 @@ if ($_GET['sid'] && ($_GET['pid'])) {
     <?php }
 }
 
-
-  // (C) ADD COMMENT
+// (C) ADD COMMENT
 if ($_POST['sid'] && ($_POST['pid'])) {
     // (C1) CHECKS
-    if (!isset($_POST['pid']) || !isset($_POST['name']) || !isset($_POST['msg'])) {
-      die("Please provide the Post ID, name, and message");
+    if (!isset($_POST['sid']) || !isset($_POST['pid']) || !isset($_POST['name']) || !isset($_POST['msg'])) {
+        die("Please provide the Post ID, name, and message");
     }
 
     // (C2) INSERT
     try {
-      $stmt = $pdo->prepare("INSERT INTO `comments` (`post_id`, `name`, `message`) VALUES (?,?,?)");
-      $stmt->execute([$_POST['pid'], htmlentities($_POST['name']), htmlentities($_POST['msg'])]);
+        $stmt = $pdo->prepare("INSERT INTO `comments` (`section_id`, `post_id`, `name`, `message`) VALUES (?,?,?,?)");
+        $stmt->execute([$_POST['sid'], $_POST['pid'], htmlentities($_POST['name']), htmlentities($_POST['msg'])]);
     } catch (Exception $ex) {
-      die($ex->getMessage());
+        die($ex->getMessage());
     }
     echo "OK";
-  }
 }
 
 // (D) CLOSE DATABASE CONNECTION
