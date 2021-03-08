91c1ca48c88ce8dc88e892da64156546852de6a9
Jens Ihrig <jens.ihrig@mail.de>
2021-03-08 21:06:00 +0100

    added new content

--- __m__/index.php
+++ __m__/index.php
@@ -1,8 +1,8 @@
 <?php
 
-$title = $_GET['title'];
+$title = $_GET['t'];
 
-define("_BBC_PAGE_NAME", $title);
+define("_BBC_PAGE_NAME", urldecode($title));
 define("_BBCLONE_DIR", "/var/www/web247/html/stats/");
 define("COUNTER", _BBCLONE_DIR . "mark_page.php");
 if (is_readable(COUNTER)) {
