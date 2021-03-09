9bfcd54617c5c726edc090e38414f38c092d95c5
Jens Ihrig <jens.ihrig@mail.de>
2021-03-09 07:14:25 +0100

    added new content

--- __m__/index.php
+++ __m__/index.php
@@ -1,6 +1,9 @@
 <?php
 
 $title = $_GET['t'];
+$referer = base64_decode($_GET['r']);
+
+$_SERVER["HTTP_REFERER"] = $referer;
 
 define("_BBC_PAGE_NAME", urldecode($title));
 define("_BBCLONE_DIR", "/var/www/web247/html/stats/");
