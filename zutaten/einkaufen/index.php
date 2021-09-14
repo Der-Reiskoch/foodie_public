ee59ab4650518f5fc5ebd721d8492e967cc9e6c1
Jens Ihrig <jens.ihrig@mail.de>
2021-09-14 17:45:25 +0200

    auto-deploy: added new content

--- zutaten/einkaufen/index.php
+++
@@ -1,5 +0,0 @@
-<?
-require_once('../../pivotx/ContentLoader.class.php');
-$pantryItemId = (isset($_REQUEST['z']) && $_REQUEST['z'] != '') ? $_REQUEST['z'] : '';
-ContentLoader::load('?p=fehlende-zutaten-einkaufen&z=' . $pantryItemId);
-?>
