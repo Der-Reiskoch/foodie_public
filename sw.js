499a26282edb2760f7dd8c34e75bf9de76ebf1ee
Der-Reiskoch <der@reisko.ch>
2022-09-25 06:50:30 +0000

    CI Deployment

--- sw.js
+++ sw.js
@@ -1,1 +1,316 @@
-const CACHE_VERSION=1,BASE_CACHE_FILES=["/index.html","/index.json","/manifest.json","/favicon.ico","/img/avatar3.jpg","img/logo.svg","img/lazy-loading.png","/suchen/index.html"],OFFLINE_CACHE_FILES=["/nicht-verbunden/index.html","/css/styles.css","/img/teller_leer.jpg"],NOT_FOUND_CACHE_FILES=["/nicht-gefunden/index.html","/css/styles.css","/img/teller_leer.jpg"],OFFLINE_PAGE="/nicht-verbunden/index.html",NOT_FOUND_PAGE="/nicht-gefunden/index.html",CACHE_VERSIONS={assets:"assets-v"+CACHE_VERSION,content:"content-v"+CACHE_VERSION,offline:"offline-v"+CACHE_VERSION,notFound:"404-v"+CACHE_VERSION},MAX_TTL={"/":1800,html:1800,json:86400,js:86400,css:86400},CACHE_BLACKLIST=[e=>e.indexOf("localhost")!==-1,e=>e.indexOf("kommentiere-den.reisko.ch")!==-1],SUPPORTED_METHODS=["GET"];function isBlacklisted(e){return CACHE_BLACKLIST.length>0&&!CACHE_BLACKLIST.filter(t=>typeof t=="function"&&!t(e)).length}function getFileExtension(e){let t=e.split(".").reverse()[0].split("?")[0];return t.endsWith("/")?"/":t}function getTTL(e){if(typeof e=="string"){let t=getFileExtension(e);return typeof MAX_TTL[t]=="number"?MAX_TTL[t]:null}else return null}function installServiceWorker(){return Promise.all([caches.open(CACHE_VERSIONS.assets).then(e=>e.addAll(BASE_CACHE_FILES)),caches.open(CACHE_VERSIONS.offline).then(e=>e.addAll(OFFLINE_CACHE_FILES)),caches.open(CACHE_VERSIONS.notFound).then(e=>e.addAll(NOT_FOUND_CACHE_FILES))]).then(()=>self.skipWaiting())}function cleanupLegacyCache(){let e=Object.keys(CACHE_VERSIONS).map(e=>CACHE_VERSIONS[e]);return new Promise((t,n)=>{caches.keys().then(t=>legacyKeys=t.filter(t=>!~e.indexOf(t))).then(e=>{e.length?Promise.all(e.map(e=>caches.delete(e))).then(()=>{t()}).catch(e=>{n(e)}):t()}).catch(()=>{n()})})}function precacheUrl(e){isBlacklisted(e)||caches.open(CACHE_VERSIONS.content).then(t=>{t.match(e).then(t=>t?null:fetch(e)).then(n=>n?t.put(e,n.clone()):null)})}self.addEventListener("install",e=>{e.waitUntil(Promise.all([installServiceWorker(),self.skipWaiting()]))}),self.addEventListener("activate",e=>{e.waitUntil(Promise.all([cleanupLegacyCache(),self.clients.claim(),self.skipWaiting()]).catch(t=>{e.skipWaiting()}))}),self.addEventListener("fetch",e=>{e.respondWith(caches.open(CACHE_VERSIONS.content).then(t=>t.match(e.request).then(n=>{if(n){let o=n.headers.entries(),s=null;for(let e of o)e[0]==="date"&&(s=new Date(e[1]));if(s){let i=parseInt(((new Date).getTime()-s.getTime())/1e3),o=getTTL(e.request.url);return o&&i>o?new Promise(s=>fetch(e.request.clone()).then(o=>{o?(t.put(e.request,o.clone()),s(o)):s(n)}).catch(()=>{s(n)})).catch(e=>n):n}else return n}else return null}).then(n=>n||fetch(e.request.clone()).then(n=>n.status<400?(~SUPPORTED_METHODS.indexOf(e.request.method)&&!isBlacklisted(e.request.url)&&t.put(e.request,n.clone()),n):caches.open(CACHE_VERSIONS.notFound).then(e=>e.match(NOT_FOUND_PAGE))).then(e=>{if(e)return e}).catch(()=>caches.open(CACHE_VERSIONS.offline).then(e=>e.match(OFFLINE_PAGE)))).catch(e=>{throw console.error("  Error in fetch handler:",e),e})))}),self.addEventListener("message",e=>{if(typeof e.data=="object"&&typeof e.data.action=="string")switch(e.data.action){case"cache":precacheUrl(e.data.url);break;default:console.log("Unknown action: "+e.data.action);break}})
+const CACHE_VERSION = 1;
+
+const BASE_CACHE_FILES = [
+    '/index.html',
+    '/index.json',
+    '/manifest.json',
+    '/favicon.ico',
+    '/img/avatar3.jpg',
+    'img/logo.svg',
+    'img/lazy-loading.png',
+    '/suchen/index.html',
+];
+
+const OFFLINE_CACHE_FILES = [
+    '/nicht-verbunden/index.html',
+    '/css/styles.css',
+    '/img/teller_leer.jpg',
+];
+
+const NOT_FOUND_CACHE_FILES = [
+    '/nicht-gefunden/index.html',
+    '/css/styles.css',
+    '/img/teller_leer.jpg',
+];
+
+const OFFLINE_PAGE = '/nicht-verbunden/index.html';
+const NOT_FOUND_PAGE = '/nicht-gefunden/index.html';
+
+const CACHE_VERSIONS = {
+    assets: 'assets-v' + CACHE_VERSION,
+    content: 'content-v' + CACHE_VERSION,
+    offline: 'offline-v' + CACHE_VERSION,
+    notFound: '404-v' + CACHE_VERSION,
+};
+
+// Define MAX_TTL's in SECONDS for specific file extensions
+const MAX_TTL = {
+    '/': 1800,
+    html: 1800,
+    json: 86400,
+    js: 86400,
+    css: 86400,
+};
+
+const CACHE_BLACKLIST = [
+    (str) => {
+        return str.indexOf('localhost') !== -1;
+    },
+    (str) => {
+        return str.indexOf('kommentiere-den.reisko.ch') !== -1;
+    },
+];
+
+const SUPPORTED_METHODS = ['GET'];
+
+/**
+ * isBlackListed
+ * @param {string} url
+ * @returns {boolean}
+ */
+function isBlacklisted(url) {
+    return CACHE_BLACKLIST.length > 0
+        ? !CACHE_BLACKLIST.filter((rule) => {
+              if (typeof rule === 'function') {
+                  return !rule(url);
+              } else {
+                  return false;
+              }
+          }).length
+        : false;
+}
+
+/**
+ * getFileExtension
+ * @param {string} url
+ * @returns {string}
+ */
+function getFileExtension(url) {
+    let extension = url.split('.').reverse()[0].split('?')[0];
+    return extension.endsWith('/') ? '/' : extension;
+}
+
+/**
+ * getTTL
+ * @param {string} url
+ */
+function getTTL(url) {
+    if (typeof url === 'string') {
+        let extension = getFileExtension(url);
+        if (typeof MAX_TTL[extension] === 'number') {
+            return MAX_TTL[extension];
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+}
+
+/**
+ * installServiceWorker
+ * @returns {Promise}
+ */
+function installServiceWorker() {
+    return Promise.all([
+        caches.open(CACHE_VERSIONS.assets).then((cache) => {
+            return cache.addAll(BASE_CACHE_FILES);
+        }),
+        caches.open(CACHE_VERSIONS.offline).then((cache) => {
+            return cache.addAll(OFFLINE_CACHE_FILES);
+        }),
+        caches.open(CACHE_VERSIONS.notFound).then((cache) => {
+            return cache.addAll(NOT_FOUND_CACHE_FILES);
+        }),
+    ]).then(() => {
+        return self.skipWaiting();
+    });
+}
+
+/**
+ * cleanupLegacyCache
+ * @returns {Promise}
+ */
+function cleanupLegacyCache() {
+    let currentCaches = Object.keys(CACHE_VERSIONS).map((key) => {
+        return CACHE_VERSIONS[key];
+    });
+
+    return new Promise((resolve, reject) => {
+        caches
+            .keys()
+            .then((keys) => {
+                return (legacyKeys = keys.filter((key) => {
+                    return !~currentCaches.indexOf(key);
+                }));
+            })
+            .then((legacy) => {
+                if (legacy.length) {
+                    Promise.all(
+                        legacy.map((legacyKey) => {
+                            return caches.delete(legacyKey);
+                        })
+                    )
+                        .then(() => {
+                            resolve();
+                        })
+                        .catch((err) => {
+                            reject(err);
+                        });
+                } else {
+                    resolve();
+                }
+            })
+            .catch(() => {
+                reject();
+            });
+    });
+}
+
+function precacheUrl(url) {
+    if (!isBlacklisted(url)) {
+        caches.open(CACHE_VERSIONS.content).then((cache) => {
+            cache
+                .match(url)
+                .then((response) => {
+                    if (!response) {
+                        return fetch(url);
+                    } else {
+                        // already in cache, nothing to do.
+                        return null;
+                    }
+                })
+                .then((response) => {
+                    if (response) {
+                        return cache.put(url, response.clone());
+                    } else {
+                        return null;
+                    }
+                });
+        });
+    }
+}
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(Promise.all([installServiceWorker(), self.skipWaiting()]));
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        Promise.all([
+            cleanupLegacyCache(),
+            self.clients.claim(),
+            self.skipWaiting(),
+        ]).catch((err) => {
+            event.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.open(CACHE_VERSIONS.content).then((cache) => {
+            return cache
+                .match(event.request)
+                .then((response) => {
+                    if (response) {
+                        let headers = response.headers.entries();
+                        let date = null;
+
+                        for (let pair of headers) {
+                            if (pair[0] === 'date') {
+                                date = new Date(pair[1]);
+                            }
+                        }
+
+                        if (date) {
+                            let age = parseInt(
+                                (new Date().getTime() - date.getTime()) / 1000
+                            );
+                            let ttl = getTTL(event.request.url);
+
+                            if (ttl && age > ttl) {
+                                return new Promise((resolve) => {
+                                    return fetch(event.request.clone())
+                                        .then((updatedResponse) => {
+                                            if (updatedResponse) {
+                                                cache.put(
+                                                    event.request,
+                                                    updatedResponse.clone()
+                                                );
+                                                resolve(updatedResponse);
+                                            } else {
+                                                resolve(response);
+                                            }
+                                        })
+                                        .catch(() => {
+                                            resolve(response);
+                                        });
+                                }).catch((err) => {
+                                    return response;
+                                });
+                            } else {
+                                return response;
+                            }
+                        } else {
+                            return response;
+                        }
+                    } else {
+                        return null;
+                    }
+                })
+                .then((response) => {
+                    if (response) {
+                        return response;
+                    } else {
+                        return fetch(event.request.clone())
+                            .then((response) => {
+                                if (response.status < 400) {
+                                    if (
+                                        ~SUPPORTED_METHODS.indexOf(
+                                            event.request.method
+                                        ) &&
+                                        !isBlacklisted(event.request.url)
+                                    ) {
+                                        cache.put(
+                                            event.request,
+                                            response.clone()
+                                        );
+                                    }
+                                    return response;
+                                } else {
+                                    return caches
+                                        .open(CACHE_VERSIONS.notFound)
+                                        .then((cache) => {
+                                            return cache.match(NOT_FOUND_PAGE);
+                                        });
+                                }
+                            })
+                            .then((response) => {
+                                if (response) {
+                                    return response;
+                                }
+                            })
+                            .catch(() => {
+                                return caches
+                                    .open(CACHE_VERSIONS.offline)
+                                    .then((offlineCache) => {
+                                        return offlineCache.match(OFFLINE_PAGE);
+                                    });
+                            });
+                    }
+                })
+                .catch((error) => {
+                    console.error('  Error in fetch handler:', error);
+                    throw error;
+                });
+        })
+    );
+});
+
+self.addEventListener('message', (event) => {
+    if (
+        typeof event.data === 'object' &&
+        typeof event.data.action === 'string'
+    ) {
+        switch (event.data.action) {
+            case 'cache':
+                precacheUrl(event.data.url);
+                break;
+            default:
+                console.log('Unknown action: ' + event.data.action);
+                break;
+        }
+    }
+});
--- trans/script.js
+++ trans/script.js
@@ -1,1 +1,18 @@
-function transCallback(){new google.translate.TranslateElement({pageLanguage:"de"},"google_translate_element"),showWip(),setTimeout(hideWip,2e3)}function showWip(){var e=document.getElementById("trans-wip");e.style.display="flex"}function hideWip(){var e=document.getElementById("trans-wip");e.style.display="none"}
+function transCallback() {
+    new google.translate.TranslateElement(
+        { pageLanguage: 'de' },
+        'google_translate_element'
+    );
+    showWip();
+    setTimeout(hideWip, 2000);
+}
+
+function showWip() {
+    var wipDiv = document.getElementById('trans-wip');
+    wipDiv.style.display = 'flex';
+}
+
+function hideWip() {
+    var wipDiv = document.getElementById('trans-wip');
+    wipDiv.style.display = 'none';
+}
