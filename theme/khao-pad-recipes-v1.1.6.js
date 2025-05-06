27b2b873bee003436d2f988bf6b6e4b3400f1e2c
Der-Reiskoch <der@reisko.ch>
2025-05-06 12:05:56 +0000

    CI Deployment

--- theme/khao-pad-recipes-v1.1.6.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill.js";import"./khao-pad-wake-lock-switch-v1.1.6.js";import"./khao-ui-svelte-dispatchRemoteControlEvent-4.8.2.js";import"./khao-ui-svelte-style-4.8.2.js";import"./khao-ui-icon-4.8.2.js";console.log("[INFO] khao-recipes: empty module loaded");
